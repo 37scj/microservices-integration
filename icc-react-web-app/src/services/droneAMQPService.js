4d1e6acf0d6e713a0d631d3a588497d1008b9b52
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2020-12-20 17:08:43 -0300

    drone amqp service

--- icc-react-web-app/src/services/droneAMQPService.js
+++ icc-react-web-app/src/services/droneAMQPService.js
@@ -1,55 +1,38 @@
 var amqp = require('amqplib');
 
-var args = process.argv.slice(2);
-var severity = (args.length > 0) ? args[0] : 'info';
+export const sendDrone=(drone)=> {
 
-// function getRandomArbitrary(min, max) {
-//     return Math.random() * (max - min) + min;
-// }
+    const sendMessage = function (ch) {
 
-// const rnd = (min, max, digits = 8) => Math.round(
-//     (Math.random() * (max - min) + min) * Math.pow(10, digits)) / Math.pow(10, digits);
-// const rnd_ceil = (min, max, digits = 2) => {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.round((Math.random() * (max - min + 1) + min) * Math.pow(10, digits)) / Math.pow(10, digits);
-// };
+        const q = 'queue.producer.mba';
+        const ex = 'projeto.mba.fiap';
 
-const sendMessage = function (ch) {
-    
-    const q = 'queue.producer.mba';
-    const ex = 'projeto.mba.fiap';
+        const msg = JSON.stringify(drone);
 
-    const msg = JSON.stringify({
-        "id": id,
-        latitude: -23.533773 + rnd(-2, 2),
-        longitude: -46.625290 + rnd(-2, 2),
-        temperatura: rnd_ceil(-25, 60),
-        umidade: rnd_ceil(0, 99)
-    });
+        var ok = ch.assertExchange(ex, 'direct', {
+            durable: true
+        });
 
-    var ok = ch.assertExchange(ex, 'direct', {
-        durable: true
-    });
+        return ok.then(function (_qok) {
+            // NB: `sentToQueue` and `publish` both return a boolean
+            // indicating whether it's OK to send again straight away, or
+            // (when `false`) that you should wait for the event `'drain'`
+            // to fire before writing again. We're just doing the one write,
+            // so we'll ignore it.
 
-    return ok.then(function (_qok) {
-        // NB: `sentToQueue` and `publish` both return a boolean
-        // indicating whether it's OK to send again straight away, or
-        // (when `false`) that you should wait for the event `'drain'`
-        // to fire before writing again. We're just doing the one write,
-        // so we'll ignore it.
-        
-            let r=ch.publish(ex, q, Buffer.from(msg))
-            console.log(" [x] Sent %s:'%s'", severity, msg, r);
-            
-        return ch.close();
-    });
-};
+            let r = ch.publish(ex, q, Buffer.from(msg));
+            console.log(" [x] Sent %s:'%s'", 'info', msg, r);
 
-amqp.connect('amqp://localhost:5672').then(function (conn) {
-    return conn.createChannel()
-        .then(sendMessage)
-        .finally(function () {
-        conn.close();
-    });
-}).catch(console.warn);
+            return ch.close();
+        });
+    };
+
+    amqp.connect('amqp://localhost:5672').then(function (conn) {
+        return conn.createChannel()
+            .then(sendMessage)
+            .finally(function () {
+                conn.close();
+            });
+    }).catch(console.warn);
+
+}
